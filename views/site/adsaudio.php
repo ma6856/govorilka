5c6c58c0bbeb2ba0ccfc044f73bd448759e9669a
ma6856 <ma685668@gmail.com>
2016-06-24 13:03:19 +0300

    first commit

---
+++ views/site/adsaudio.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'adsaudio';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        Страница с аудиорекламой.
+    </p>
+
+    <code><?= __FILE__ ?></code>
+</div>
